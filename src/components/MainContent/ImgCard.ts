122fc523cc02cbe99f8849ea844a2743a5a4d9d6
kimsubni <yosopu0105@naver.com>
2023-06-30 10:05:23 +0900

    Feat : #13 img, video component date 추가

--- src/components/MainContent/ImgCard.ts
+++ src/components/MainContent/ImgCard.ts
@@ -7,13 +7,17 @@ type ImgTypeProps = {
 export default class ImgCard extends Component<ImgTypeProps, StateType> {
   template(): string {
     const { url, title, tag, description } = this.props.item;
+    const date = this.props.item.date.toLocaleDateString();
+
     return `
-      <div class="imgcard-wrapper">
+      <div class="imgvideocard-wrapper">
         <img src=${url} class='img-url'>
-        <div class="img-desc">
-          <div class="title">
-            ${title}
+        <div class="imgvideo-desc">
+          <div class="imgvideo-header">
+            <div class="title"> ${title}&nbsp;&nbsp; </div>
+            <div class="date">${date} </div>
           </div>
+      
           <div class="description">
             ${description}
           </div>
--- src/components/MainContent/VideoCard.ts
+++ src/components/MainContent/VideoCard.ts
@@ -6,14 +6,17 @@ type VideoTypeProps = {
 export default class VideoCard extends Component<VideoTypeProps, StateType> {
   template(): string {
     const { url, title, tag, description } = this.props.item;
+    const date = this.props.item.date.toLocaleDateString();
 
-    return ` <div class="imgcard-wrapper">
-        <iframe width="280" height="180" src=${url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    return ` <div class="imgvideocard-wrapper">
+        <iframe class="video" src=${url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
-        <div class="img-desc">
-          <div class="title">
-            ${title}
+        <div class="imgvideo-desc">
+          <div class="imgvideo-header">
+            <div class="title"> ${title}&nbsp;&nbsp; </div>
+            <div class="date">${date} </div>
           </div>
+          
           <div class="description">
             ${description}
           </div>
